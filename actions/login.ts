30f3d390beada12dd18549a613747bc539107a8e
yurixtugal <yurixtugal12@gmail.com>
2024-01-12 01:14:36 -0500

    add log in with credentials

--- actions/login.ts
+++ actions/login.ts
@@ -2,14 +2,33 @@
 
 import * as z from 'zod'
 
+import { signIn } from '@/auth'
 import { LoginSchema } from '@/schemas'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { AuthError } from 'next-auth'
 
 export const login = async (value: z.infer<typeof LoginSchema>) => {
   const validate = LoginSchema.safeParse(value)
-  console.log(validate)
   if (!validate.success) {
     return { error: "Invalid fidelds"}
   }
 
-  return { success: "Email sent"}
+  const { email, password } = validate.data
+
+  try{
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    })
+
+  } catch (error) {
+    if (error instanceof AuthError){
+      switch (error.type){
+        case "CredentialsSignin": return { error: "Invalid credentials"};
+        default: return { error: "Something went wrong"};
+      }
+    }
+    throw error;
+  }
 }
---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,22 @@
+import { auth } from "@/auth";
+import { signOut } from "@/auth";
+
+const SettingsPage = async () => {
+  const session = await auth();
+  return (
+    <div>
+      {JSON.stringify(session, null, 2)}
+      <form action={
+        async () => {
+          "use server";
+
+          await signOut();
+        }
+      }>
+        <button type="submit">Logout</button>
+      </form>
+    </div>
+    );
+}
+ 
+export default SettingsPage;
