c44e4253c9b7c711239defacee16d713176d7eb1
yurixtugal <yurixtugal12@gmail.com>
2024-01-12 00:02:33 -0500

    Register user basic information and DB integration

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,39 @@
 "use server"
 
+import bcrypt from 'bcrypt'
 import * as z from 'zod'
 
 import { RegisterSchema } from '@/schemas'
 
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
+
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
   const validate = RegisterSchema.safeParse(value)
-  console.log(validate)
   if (!validate.success) {
     return { error: "Invalid fidelds"}
   }
 
-  return { success: "Email sent"}
+  const { email, password, firstName, lastName } = validate.data
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists"}
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName
+    }
+  })
+
+  // send verification user email
+
+  return { success: "User created" }
 }
